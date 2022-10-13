4e39c63e95a6bcedad5d1d1c0fd49dc58e39e8f1
mukhinvictor777 <mukhinvictor@gmail.com>
2022-10-14 00:52:51 +0600

    task32 заменил знак в элементах массива

--- task32/Program.cs
+++ task32/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿int[] getRandomArray (int Length, int startPoint, int endPoint)
+{   
+    int [] resultArray = new int[Length];
+    for (int i = 0; i < Length; i++)
+    {
+        resultArray[i] = new Random().Next(startPoint, endPoint + 1);
+    }
+    return resultArray;  
+}
+
+void printArray(int[] incomingArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if (i < incomingArray.Length - 1)
+        {
+            Console.Write(",");
+        }       
+    }
+    Console.WriteLine("]");
+}
+
+int[] getInversionArray (int[] incomingArray)
+{
+  
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        incomingArray[i] = incomingArray[i] * -1;
+    }
+    return incomingArray;
+}
+
+int [] currentArray = getRandomArray (10, -9, 9);
+Console.WriteLine();
+printArray(currentArray);
+int [] inversionArray = getInversionArray(currentArray);
+Console.WriteLine();
+printArray(inversionArray);
+Console.WriteLine();
